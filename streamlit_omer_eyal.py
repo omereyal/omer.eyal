79cd2d4c833999d772d508f9215fe571b94664de
omereyal <118986047+omereyal@users.noreply.github.com>
2022-12-08 18:53:21 +0200

    Update streamlit_omer_eyal.py

--- streamlit_omer_eyal.py
+++ streamlit_omer_eyal.py
@@ -2,7 +2,6 @@
 # import libs
 import streamlit as st
 from skimage import measure, io, img_as_ubyte, morphology, util, color
-import matplotlib.pyplot as plt
 from skimage.color import label2rgb, rgb2gray
 import numpy as np
 import pandas as pd
